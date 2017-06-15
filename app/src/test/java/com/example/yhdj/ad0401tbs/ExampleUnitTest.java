bb238bc647b52f883a8e682fc16e0ebfdf5d2f23
kai <1145275516@qq.com>
2017-06-15 09:03:27 +0800

    pengkai

---
+++ app/src/test/java/com/example/yhdj/ad0401tbs/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package com.example.yhdj.ad0401tbs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
